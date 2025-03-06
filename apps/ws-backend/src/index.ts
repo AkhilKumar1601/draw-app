5398e390081541fc868ebb1c8bc80ef53c38bc98
AkhilKumar1601 <akhilchaudhary1601@gmail.com>
2025-03-06 17:17:50 +0530

    Web socket backend

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,6 +1,7 @@
 import { WebSocket,WebSocketServer } from "ws";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config"; 
+import { prismaClient } from "@repo/db/client";
 
 const wss = new WebSocketServer({ port : 8080});
 
@@ -51,6 +52,46 @@ wss.on('connection', function connection(ws,request) {
     ws,
     rooms : [],
     userId
+  })
+
+  ws.on('/message', async function message(data) {
+    const parshedData = JSON.parse(data as unknown as string);
+
+    if(parshedData.type === "join_room") {
+      const user = users.find(x => x.ws === ws);
+      user?.rooms.push(parshedData.roomId);
+    }
+
+    if(parshedData.type === "leave_room") {
+      const user = users.find(x => x.ws === ws);
+      if(!user) {
+        return;
+      }
+      user.rooms = user.rooms.filter(x => x === parshedData.room);
+    }
+
+    if(parshedData.type === "chat") {
+      const roomId = parshedData.roomId;
+      const message = parshedData.message;
+
+      await prismaClient.chat.create({
+         data : {
+          roomId : roomId,
+          message : message,
+          userId : userId
+         }
+      });
+
+      users.forEach(user => {
+        if(user.rooms.includes(roomId)) {
+          user.ws.send(JSON.stringify({
+            type : "chat",
+            message : message,
+            roomId : roomId
+          }))
+        }
+      })
+    }
   })
 
 })
