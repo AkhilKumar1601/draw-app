92b7aa5553ab090378eeae0b69f0b60bc5f110af
AkhilKumar1601 <akhilchaudhary1601@gmail.com>
2025-03-10 01:31:02 +0530

    landing page, sigin and signup page

--- apps/excelidraw-frontend/components/AuthPage.tsx
+++ apps/excelidraw-frontend/components/AuthPage.tsx
@@ -4,29 +4,31 @@ export default function AuthPage({isSignin} : {
   isSignin : boolean
 }) {
 
-  return (
+   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="p-6 m-2 bg-white rounded">
-          <div className="p-2">
-          <input type="text" placeholder="Email" className="text-black"></input>
-          </div>
-          
-          <div className="p-2">
-          <input type="password" placeholder="Enter your password" className="text-black"/>
-          </div>
-          
-          <div>
-            <button
-            className="pt-2 text-black"
-            onClick={() => {
 
-            }}
-            >
-              {isSignin ? "Sign in" : "Signup"}
-            </button>
-          </div>
+      <div className="p-6 m-2 bg-white rounded">
+
+        <div className="p-2">
+          <input type="text" placeholder="Email" className="text-black" />
+        </div>
+
+        <div className="p-2">
+          <input type="password" placeholder="Enter your password" className="text-black" />
+        </div>
+        
+
+        <div className="pt-2">
+          <button onClick={() => {
+
+          }} className="text-black">
+            {isSignin ? "Sign in" : "Sign up"}
+          </button>
+        </div>
+
       </div>
+      
     </div>
-  )
+   )
 
 }
