a97707a39bca5a82fec993c3d440c7d5328f62f0
AkhilKumar1601 <akhilchaudhary1601@gmail.com>
2025-03-13 14:31:47 +0530

    Added Circle Shape and structure our code better

--- apps/excelidraw-frontend/components/Canvas.tsx
+++ apps/excelidraw-frontend/components/Canvas.tsx
@@ -1,5 +1,10 @@
-import initDraw from "@/draw";
-import { useEffect, useRef } from "react";
+import { initDraw } from "@/draw";
+import { useEffect, useRef, useState } from "react";
+import { IconButton } from "./IconButton";
+import { Circle, Pencil, RectangleHorizontalIcon } from "lucide-react";
+import { Game } from "@/draw/Game";
+
+export type Tool = "circle"  | "rect" | "pencil";
 
 export function Canvas({
   roomId,
@@ -9,18 +14,76 @@ export function Canvas({
   socket : WebSocket
 }) {
 
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+   const canvasRef = useRef<HTMLCanvasElement>(null);
+   const [game,setGame] = useState<Game>();
+   const [selectedTool,setSelectedTool] = useState<Tool>("circle");
 
-  useEffect(() => {
+   useEffect(() => {
+       game?.setTool(selectedTool);
+   }, [selectedTool,game]);
 
-    if(canvasRef.current) {
-      initDraw(canvasRef.current,roomId,socket);
-    }
+   useEffect(() => {
+      if(canvasRef.current) {
+        initDraw(canvasRef.current,roomId,socket);
+        const g = new Game(canvasRef.current,roomId, socket);
+        setGame(g);
 
-  }, [canvasRef]);
+        return () => {
+          g.destroy();
+        }
+      }
+   }, [canvasRef])
+   
+   return <div style = {{
+    height: "100vh",
+    overflow:"hidden"
+   }}>
 
-  return <div>
-    <canvas ref={canvasRef} width={2000} height={1000}></canvas>
-  </div>
+    <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight}></canvas>
 
+    <TopBar setSelectedTool={setSelectedTool} selectedTool={selectedTool}/>
+
+   </div>
+
+}
+
+function TopBar({selectedTool, setSelectedTool} : {
+   selectedTool : Tool,
+   setSelectedTool : (s : Tool) => void
+}) {
+   return <div style={{
+    position : "fixed",
+    top : 10,
+    left : 10
+   }}>
+
+      <div className="flex gap-t">
+
+         <IconButton 
+            onClick = { () => {
+                 setSelectedTool("pencil")
+            }}
+            activated={selectedTool === "pencil"}
+            icon={<Pencil />}
+         />
+
+         <IconButton 
+            onClick = { () => {
+                  setSelectedTool("rect")
+            }}
+            activated={selectedTool=== "rect"}
+            icon={<RectangleHorizontalIcon />}
+         />
+
+         <IconButton 
+            onClick = {() => {
+                 setSelectedTool("circle")
+            }}
+            activated={selectedTool==="circle"}
+            icon={<Circle />}
+         />
+
+      </div>
+
+   </div>
 }
